9337321a5ab7a6fecb1c48f8a5183c94ab0b383c
indra <indrasyahputra048@gmail.com>
2017-07-10 17:16:33 +0700

    Pembagian template admin

---
+++ admin/template/datapelangganfooter.php
@@ -0,0 +1,18 @@
+
+    <div class="footer" id="footer">
+        <p>&copy; Sisfo 2015</p>
+    </div>
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+    function getConfirmation() {
+        var retVal = confirm("Do you want to continue ?");
+        if (retVal == true) {
+
+            return true;
+        }
+    }
+    </script>
+</body>
+
+</html>
---
+++ admin/template/datapelanggansidebar.php
@@ -0,0 +1,28 @@
+    <div class="container-fluid" id="container">
+        <div class="row">
+            <div class="col-md-2" id="side-left">
+                <ul class="nav nav-pills nav-stacked" role="tablist">
+                    <li class="active">
+                        <a href="datapelanggan.html"><i class="glyphicon glyphicon-user"></i> Data Master<span class="fa arrow"></span></a>
+                        <ul class="nav nav-second-level">
+                            <li>
+                                <a class="active" href="datapelanggan.html">Data Pelanggan</a>
+                            </li>
+                            <li>
+                                <a href="tambahpelanggan.html" id="loginmenu">Tambah Data Pelanggan</a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="glyphicon glyphicon-file"></i> Laporan</a>
+                                <ul class="nav-second-level">
+                                    <li>
+                                        <a href="laporansukucadangcs.html">Laporan data Suku Cadang</a>
+                                    </li>
+                                    <li>
+                                        <a href="laporanservice.html">Laporan Service</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
